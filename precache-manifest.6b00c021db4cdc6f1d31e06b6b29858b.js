56bfa9f19090d9be1b61c358f18bbd338fa7e3a4
Vylhem <58944443+Vylhem@users.noreply.github.com>
2019-12-16 22:05:00 +0300

    Updates

---
+++ precache-manifest.6b00c021db4cdc6f1d31e06b6b29858b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b106737e30bc5e1236a0b19dab3342",
+    "url": "./index.html"
+  },
+  {
+    "revision": "513fa2e93ca1469fa205",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "c48effad9816f98632e8",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "513fa2e93ca1469fa205",
+    "url": "./static/js/2.ba7186e6.chunk.js"
+  },
+  {
+    "revision": "c48effad9816f98632e8",
+    "url": "./static/js/main.3fe41bb1.chunk.js"
+  },
+  {
+    "revision": "c97d1b9cc7950846926a",
+    "url": "./static/js/runtime-main.0c651eed.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.471ce3062144242c5d17765ce5dab8ca.js"
+  "./precache-manifest.6b00c021db4cdc6f1d31e06b6b29858b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
